9af0b9eafc28a3482261df192d74cfccbbd816c4
Kevin Schaper <kevinschaper@gmail.com>
2021-06-25 15:14:44 -0700

    Added Xenbase

---
+++ mingestibles/Xenbase/gene-information.py
@@ -0,0 +1,16 @@
+import logging
+
+from koza.converter.biolink_converter import gpi2gene
+from koza.manager.data_collector import write
+from koza.manager.data_provider import inject_row
+
+LOG = logging.getLogger(__name__)
+
+source_name = "gene-information"
+row = inject_row(source_name)
+
+row["DB_Object_ID"] = "Xenbase:" + row["DB_Object_ID"]
+
+gene = gpi2gene(row)
+
+write(source_name, gene)
---
+++ mingestibles/Xenbase/gene-to-phenotype.py
@@ -0,0 +1,28 @@
+import uuid
+
+from biolink_model_pydantic.model import (
+    Gene,
+    GeneToPhenotypicFeatureAssociation,
+    PhenotypicFeature,
+    Predicate,
+)
+from koza.manager.data_collector import write
+from koza.manager.data_provider import inject_row
+
+source_name = "gene-to-phenotype"
+
+row = inject_row(source_name)
+
+gene = Gene(id="Xenbase:" + row["SUBJECT"])
+
+phenotype = PhenotypicFeature(id=row["OBJECT"])
+
+association = GeneToPhenotypicFeatureAssociation(
+    id="uuid:" + str(uuid.uuid1()),
+    subject=gene.id,
+    predicate=Predicate.has_phenotype,
+    object=phenotype.id,
+    relation=row["RELATION"].replace("_", ":"),
+)
+
+write(source_name, gene, association, phenotype)
