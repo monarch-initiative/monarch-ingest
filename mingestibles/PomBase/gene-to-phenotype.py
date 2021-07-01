366359c73c036a828df7ca1d81d0b16be05bd26f
Kevin Schaper <kevinschaper@gmail.com>
2021-07-01 15:31:32 -0700

    Added experimental conditions as qualifiers, moved ingest logic & column list into the ingest script and removed the standard format calls

--- mingestibles/PomBase/gene-to-phenotype.py
+++ mingestibles/PomBase/gene-to-phenotype.py
@@ -1,8 +1,6 @@
-from koza.converter.biolink_converter import (
-    phaf2gene,
-    phaf2gene_pheno_association,
-    phaf2phenotype,
-)
+import uuid
+
+from biolink_model_pydantic.model import Gene, PhenotypicFeature, GeneToPhenotypicFeatureAssociation, Predicate
 from koza.manager.data_collector import write
 from koza.manager.data_provider import inject_row, inject_translation_table
 
@@ -11,10 +9,18 @@ source_name = "gene-to-phenotype"
 translation_table = inject_translation_table()
 row = inject_row(source_name)
 
-gene = phaf2gene(row, "POMBASE:", "NCBITaxon:")
-phenotype = phaf2phenotype(row)
-association = phaf2gene_pheno_association(
-    row, gene, phenotype, translation_table.resolve_term("has phenotype")
+gene = Gene(id="POMBASE:" + row['Gene systematic ID'])
+phenotype = PhenotypicFeature(id=row['FYPO ID'])
+association = GeneToPhenotypicFeatureAssociation(
+    id="uuid:" + str(uuid.uuid1()),
+    subject=gene.id,
+    predicate=Predicate.has_phenotype,
+    object=phenotype.id,
+    relation=translation_table.resolve_term('has phenotype'),
+    publications=[row['Reference']],
 )
+
+if row['Condition']:
+    association.qualifiers = row['Condition'].split(',')
 
 write(source_name, gene, phenotype, association)
