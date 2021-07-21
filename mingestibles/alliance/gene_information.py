718ecf080599793a26c0e56af57ef24ea8cd315b
Kevin Schaper <kevinschaper@gmail.com>
2021-07-20 17:53:59 -0700

    formatting

--- mingestibles/alliance/gene_information.py
+++ mingestibles/alliance/gene_information.py
@@ -1,31 +1,34 @@
 from biolink_model_pydantic.model import Gene
-from koza.manager.data_provider import inject_row, inject_curie_cleaner
 from koza.manager.data_collector import write
+from koza.manager.data_provider import inject_curie_cleaner, inject_row
 
 curie_cleaner = inject_curie_cleaner()
 
-source_name = 'gene-information'
+source_name = "gene-information"
 
 row = inject_row(source_name)
 
 # curie prefix as source?
-source = row['basicGeneticEntity']['primaryId'].split(':')[0]
+source = row["basicGeneticEntity"]["primaryId"].split(":")[0]
 
-if 'name' not in row.keys():
-    row['name'] = row['symbol']
+if "name" not in row.keys():
+    row["name"] = row["symbol"]
 
 gene = Gene(
-    id=row['basicGeneticEntity']['primaryId'],
-    symbol=row['symbol'],
-    name=row['name'],
-    type=row['soTermId'],
-    in_taxon=row['basicGeneticEntity']['taxonId'],
+    id=row["basicGeneticEntity"]["primaryId"],
+    symbol=row["symbol"],
+    name=row["name"],
+    type=row["soTermId"],
+    in_taxon=row["basicGeneticEntity"]["taxonId"],
     source=source,
 )
 
-if row['basicGeneticEntity']['crossReferences']:
-    gene.xrefs = [curie_cleaner.clean(xref['id']) for xref in row['basicGeneticEntity']['crossReferences']]
-if 'synonyms' in row['basicGeneticEntity'].keys():
-    gene.synonym = row['basicGeneticEntity']['synonyms']
+if row["basicGeneticEntity"]["crossReferences"]:
+    gene.xrefs = [
+        curie_cleaner.clean(xref["id"])
+        for xref in row["basicGeneticEntity"]["crossReferences"]
+    ]
+if "synonyms" in row["basicGeneticEntity"].keys():
+    gene.synonym = row["basicGeneticEntity"]["synonyms"]
 
 write(source_name, gene)
