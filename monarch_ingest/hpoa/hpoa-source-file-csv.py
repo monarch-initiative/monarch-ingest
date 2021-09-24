025a2f7510a9d03dc2744831864fad6e97f44ee7
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2021-09-24 10:05:48 -0700

    initial setup of hpoa

---
+++ monarch_ingest/hpoa/hpoa-source-file-csv.py
@@ -0,0 +1,28 @@
+from biolink_model_pydantic.model import Gene
+from koza.manager.data_collector import write
+from koza.manager.data_provider import inject_curie_cleaner, inject_row
+
+# You've got 'NCBI_Gene:' and you want 'NCBIGene:'? clean it up.
+curie_cleaner = inject_curie_cleaner()
+
+# The source name is used for reading and writing
+source_name = "hpoa"
+
+# inject a single row from the source
+row = inject_row(source_name)
+
+
+
+#
+# # create your entities
+# gene = Gene(
+#     id='somethingbase:'+row['ID'],
+#     name=row['Name']
+# )
+#
+# # populate any additional optional properties
+# if row['xrefs']:
+#     gene.xrefs = [curie_cleaner.clean(xref) for xref in row['xrefs']]
+#
+# # remember to supply the source name as the first argument, followed by entities
+# write(source_name, gene)
