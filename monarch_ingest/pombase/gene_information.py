217fb377c97ed0ff92e3621bd880bd8f35249d6a
Kevin Schaper <kevinschaper@gmail.com>
2021-11-03 12:25:37 -0700

    Added full set of gene nodes for PomBase

---
+++ monarch_ingest/pombase/gene_information.py
@@ -0,0 +1,27 @@
+import logging
+
+from biolink_model_pydantic.model import Gene
+from koza.cli_runner import koza_app
+
+LOG = logging.getLogger(__name__)
+
+source_name = "pombase_gene_information"
+row = koza_app.get_row(source_name)
+
+# Given that this file is coming from PomBase, I think the biolink model is wrong about all caps
+row["curie"] = row["curie"].replace("PomBase", "POMBASE")
+
+gene = Gene(
+    id=row["curie"],
+    symbol=row["primary gene name"],
+    type=koza_app.translation_table.resolve_term(row["product type"].replace(' ', '_')),
+    source="PomBase"
+)
+
+if row["UniProtKB accession"]:
+    gene.xref = ["UniProtKB:" + row["UniProtKB accession"]]
+
+if row["synonyms"]:
+    gene.synonym = row["synonyms"].split(",")
+
+koza_app.write(gene)
---
+++ tests/unit/pombase/test_pombase_gene_information.py
@@ -0,0 +1,45 @@
+import pytest
+
+
+@pytest.fixture
+def gene_information_entities(mock_koza, global_table):
+
+    row = {
+        'systematic ID': 'SPAC1002.06c',
+        'curie': 'POMBASE:SPAC1002.06c',
+        'primary gene name': 'bqt2',
+        'chromosome': 'chromosome_1',
+        'product description': 'bouquet formation protein Bqt2',
+        'UniProtKB accession': 'Q9US52',
+        'product type': 'protein coding gene',
+        'synonyms': 'mug18,rec23',
+    }
+
+    return mock_koza(
+        "pombase_gene_information",
+        iter([row]),
+        "./monarch_ingest/pombase/gene_information.py",
+        global_table=global_table,
+    )
+
+
+def test_gene_information_gene(gene_information_entities):
+    assert len(gene_information_entities) == 1
+    gene = gene_information_entities[0]
+    assert gene
+
+
+def test_gene_information_synonym(gene_information_entities):
+    gene = gene_information_entities[0]
+    assert gene.synonym
+    assert "rec23" in gene.synonym
+
+
+def test_gene_information_id(gene_information_entities):
+    gene = gene_information_entities[0]
+    assert gene.id == "POMBASE:SPAC1002.06c"
+
+
+def test_gene_xref(gene_information_entities):
+    gene = gene_information_entities[0]
+    assert "UniProtKB:Q9US52" in gene.xref
