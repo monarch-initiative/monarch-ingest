f437725cfc57f65c63134eb097f56ba38175ce7b
Kevin Schaper <kevinschaper@gmail.com>
2021-11-22 14:56:50 -0800

    Remove fake non-output from Dagster ingest ops

--- pipeline.py
+++ pipeline.py
@@ -6,97 +6,85 @@ from koza.cli_runner import transform_source
 def ingest_xenbase_gene_literature():
     transform_source("./monarch_ingest/xenbase/gene_literature.yaml",
                      "output", "tsv", None, None)
-    return 1 # This is a hack, we don't really have natural return values, but Dagster wants them
 
 
 @op
 def ingest_xenbase_gene_information():
     transform_source("./monarch_ingest/xenbase/gene_information.yaml",
                      "output", "tsv", None, None)
-    return 1 # This is a hack, we don't really have natural return values, but Dagster wants them
 
 
 @op
 def ingest_xenbase_gene2phenotype():
     transform_source("./monarch_ingest/xenbase/gene2phenotype.yaml",
                      "output", "tsv", None, None)
-    return 1 # This is a hack, we don't really have natural return values, but Dagster wants them
 
 
 @op
 def ingest_alliance_literature():
     transform_source("./monarch_ingest/alliance/literature.yaml",
                      "output", "tsv", None, None)
-    return 1 # This is a hack, we don't really have natural return values, but Dagster wants them
 
 
 @op
 def ingest_alliance_gene_information():
     transform_source("./monarch_ingest/alliance/gene_information.yaml",
                      "output", "tsv", None, None)
-    return 1 # This is a hack, we don't really have natural return values, but Dagster wants them
+
 
 # @op
 # def ingest_alliance_gene2disease():
 #     transform_source("./monarch_ingest/alliance/gene2disease.yaml",
 #                      "output", "tsv", None, None)
-#     return 1 # This is a hack, we don't really have natural return values, but Dagster wants them
+
 
 
 @op
 def ingest_alliance_gene2phenotype():
     transform_source("./monarch_ingest/alliance/gene2phenotype.yaml",
                      "output", "tsv", None, None)
-    return 1 # This is a hack, we don't really have natural return values, but Dagster wants them
 
 
 @op
 def ingest_rgd_gene2publication():
     transform_source("./monarch_ingest/rgd/gene2publication.yaml",
                      "output", "tsv", None, None)
-    return 1 # This is a hack, we don't really have natural return values, but Dagster wants them
 
 
 @op
 def ingest_hgnc_gene_information():
     transform_source("./monarch_ingest/hgnc/gene_information.yaml",
                      "output", "tsv", None, None)
-    return 1 # This is a hack, we don't really have natural return values, but Dagster wants them
 
 
 @op
 def ingest_flybase_gene2publication():
     transform_source("./monarch_ingest/flybase/gene2publication.yaml",
                      "output", "tsv", None, None)
-    return 1 # This is a hack, we don't really have natural return values, but Dagster wants them
 
 
 @op
 def ingest_sgd_gene2publication():
     transform_source("./monarch_ingest/sgd/gene2publication.yaml",
                      "output", "tsv", None, None)
-    return 1 # This is a hack, we don't really have natural return values, but Dagster wants them
 
 
 @op
 def ingest_pombase_gene2phenotype():
     transform_source("./monarch_ingest/pombase/gene2phenotype.yaml",
                      "output", "tsv", None, None)
-    return 1 # This is a hack, we don't really have natural return values, but Dagster wants them
 
 
 @op
 def ingest_zfin_gene2publication():
     transform_source("./monarch_ingest/zfin/gene2publication.yaml",
                      "output", "tsv", None, None)
-    return 1 # This is a hack, we don't really have natural return values, but Dagster wants them
 
 
 @op
 def ingest_zfin_gene2phenotype():
     transform_source("./monarch_ingest/zfin/gene2phenotype.yaml",
                      "output", "tsv", None, None)
-    return 1 # This is a hack, we don't really have natural return values, but Dagster wants them
 
 
 @op
