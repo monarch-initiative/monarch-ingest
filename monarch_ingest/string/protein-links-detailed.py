a8e40e97571199bc29d1e1e4f19392852819499d
Richard Bruskiewich <richard@starinformatics.com>
2022-01-10 14:52:14 -0800

    Copied over Koza example files from https://github.com/monarch-initiative/koza/tree/main/examples/string-declarative; Deleted most of the source templated files

---
+++ monarch_ingest/string/protein-links-detailed.py
@@ -0,0 +1,21 @@
+import re
+import uuid
+
+from biolink_model_pydantic.model import PairwiseGeneToGeneInteraction, Predicate, Protein
+
+from koza.cli_runner import koza_app
+
+row = koza_app.get_row()
+
+protein_a = Protein(id='ENSEMBL:' + re.sub(r'\d+\.', '', row['protein1']))
+protein_b = Protein(id='ENSEMBL:' + re.sub(r'\d+\.', '', row['protein2']))
+
+pairwise_gene_to_gene_interaction = PairwiseGeneToGeneInteraction(
+    id="uuid:" + str(uuid.uuid1()),
+    subject=protein_a.id,
+    object=protein_b.id,
+    predicate=Predicate.interacts_with,
+    relation=koza_app.translation_table.global_table['interacts with'],
+)
+
+koza_app.write(protein_a, protein_b, pairwise_gene_to_gene_interaction)
