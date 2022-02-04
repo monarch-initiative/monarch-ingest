8650a6f1d6d9e529b645ce6084482e172f5eeb91
Kevin Schaper <kevinschaper@gmail.com>
2022-02-03 19:37:43 -0800

    Updated script path in test

--- tests/unit/panther/test_ref_genome_orthologs.py
+++ tests/unit/panther/test_ref_genome_orthologs.py
@@ -17,7 +17,7 @@ def script():
     """
     :return: string path to Panther Gene Orthology relationships ingest script
     """
-    return "./monarch_ingest/orthology/ref_genome_orthologs.py"
+    return "./monarch_ingest/panther/ref_genome_orthologs.py"
 
 
 @pytest.fixture
@@ -92,7 +92,7 @@ def test_rows():
 def basic_pl(mock_koza, source_name, test_rows, script, global_table, map_cache):
     """
     Mock Koza run for Panther gene orthology data ingest.
-    
+
     :param mock_koza:
     :param source_name:
     :param test_rows: a method returning a List of test data dictionary records
@@ -123,9 +123,9 @@ result_expected = {
 
 
 def test_genes(basic_pl):
-    
+
     gene = basic_pl[0]
-    
+
     assert gene
     assert gene.id in result_expected.keys()
 
@@ -141,7 +141,7 @@ def test_genes(basic_pl):
     assert "infores:entrez" in gene.source
 
     ortholog = basic_pl[1]
-    
+
     assert ortholog
     assert ortholog.id == result_expected[gene.id][1]
 
