5f648985d5300708aed90d59e762cb425936b8c2
Richard Bruskiewich <richard@starinformatics.com>
2022-02-21 14:04:31 -0800

    Adjusted Panther orthology unit tests to expect raw gene id's parsed from the Panther entries themselves (coerced to CURIE syntax)

--- tests/unit/panther/test_ref_genome_orthologs.py
+++ tests/unit/panther/test_ref_genome_orthologs.py
@@ -21,23 +21,56 @@ def script():
 
 
 @pytest.fixture
-def map_cache():
-    """
-    :return: Multi-level mock map_cache Uniprot to Entrez GeneID dictionary (realistic looking but synthetic data)
-    """
-    uniprot_2_gene = {
-        "Q15528": {"Entrez": "6837"},
-        "Q62276": {"Entrez": "20933"},
-    }
-    return {"uniprot_2_gene": uniprot_2_gene}
-
-
-@pytest.fixture
 def test_rows():
     """
-    :return: List of test GO Annotation data rows (realistic looking but synthetic data).
+    :return: List of test Panther RefGenome ortholog entries (mostly real sample but some erroneous synthetic data).
     """
     return [
+        # Well behaved records
+        {
+            "Gene": "HUMAN|HGNC=11477|UniProtKB=Q6GZX4",
+            "Ortholog": "RAT|RGD=1564893|UniProtKB=Q6GZX2",
+            "Type of ortholog": "LDO",
+            "Common ancestor for the orthologs": "Euarchontoglires",
+            "Panther Ortholog ID": "PTHR12434"
+        },
+        {
+            "Gene": "HUMAN|HGNC=11477|UniProtKB=Q6GZX4",
+            "Ortholog": "CHICK|Ensembl=ENSGALG00000003204|UniProtKB=Q197F8",
+            "Type of ortholog": "LDO",
+            "Common ancestor for the orthologs": "Amniota",
+            "Panther Ortholog ID": "PTHR12434"
+        },
+        {
+            "Gene": "HUMAN|HGNC=11477|UniProtKB=Q6GZX4",
+            "Ortholog": "DANRE|ZFIN=ZDB-GENE-040625-156|UniProtKB=Q6GZX1",
+            "Type of ortholog": "LDO",
+            "Common ancestor for the orthologs": "Euteleostomi",
+            "Panther Ortholog ID": "PTHR12434"
+        },
+        {
+            "Gene": "HUMAN|HGNC=11477|UniProtKB=Q6GZX4",
+            "Ortholog": "CAEEL|WormBase=WBGene00007022|UniProtKB=Q197F5",
+            "Type of ortholog": "LDO",
+            "Common ancestor for the orthologs": "Bilateria",
+            "Panther Ortholog ID": "PTHR12434"
+        },
+        {
+            "Gene": "HUMAN|HGNC=11477|UniProtKB=Q6GZX4",
+            "Ortholog": "DROME|FlyBase=FBgn0052971|UniProtKB=Q6GZX0",
+            "Type of ortholog": "O",
+            "Common ancestor for the orthologs": "Bilateria",
+            "Panther Ortholog ID": "PTHR12434"
+        },
+        {
+            "Gene": "HUMAN|HGNC=11477|UniProtKB=Q6GZX4",
+            "Ortholog": "DROME|FlyBase=FBgn0040339|",
+            "Type of ortholog": "LDO",
+            "Common ancestor for the orthologs": "Bilateria",
+            "Panther Ortholog ID": "PTHR12434"
+         },
+    
+        # Odd MGI Gene ID
         {
             "Gene": "HUMAN|HGNC=11477|UniProtKB=Q15528",                # species1|DB=id1|protdb=pdbid1
             "Ortholog": "MOUSE|MGI=MGI=98446|UniProtKB=Q62276",         # species2|DB=id2|protdb=pdbid2
@@ -47,22 +80,6 @@ def test_rows():
         },
         # Most of these other test rows don't generate any results
         # in the unit tests, but will generated logger messages?
-        {
-            # missing protein spec?
-            "Gene": "HUMAN|HGNC=11477|",
-            "Ortholog": "MOUSE|MGI=MGI=98446|UniProtKB=Q62276",
-            "Type of ortholog": "LDO",
-            "Common ancestor for the orthologs": "Euarchontoglires",
-            "Panther Ortholog ID": "PTHR12434"
-        },
-        {
-            # Unmappable protein ID?
-            "Gene": "HUMAN|HGNC=11477|UniProtKB=A123B3",
-            "Ortholog": "MOUSE|MGI=MGI=98446|UniProtKB=Q62276",
-            "Type of ortholog": "LDO",
-            "Common ancestor for the orthologs": "Euarchontoglires",
-            "Panther Ortholog ID": "PTHR12434"
-        },
         {
             # Non-target species ("AARDvark, lol")
             "Gene": "HUMAN|HGNC=11477|UniProtKB=Q15528",
@@ -89,7 +106,7 @@ def test_rows():
 
 
 @pytest.fixture
-def basic_pl(mock_koza, source_name, test_rows, script, global_table, map_cache):
+def basic_pl(mock_koza, source_name, test_rows, script, global_table):
     """
     Mock Koza run for Panther gene orthology data ingest.
 
@@ -98,23 +115,52 @@ def basic_pl(mock_koza, source_name, test_rows, script, global_table, map_cache)
     :param test_rows: a method returning a List of test data dictionary records
     :param script:
     :param global_table:
-    :param map_cache:
     :return:
     """
     return mock_koza(
         name=source_name,
         data=iter(test_rows),
         transform_code=script,
-        map_cache=map_cache,
         global_table=global_table
     )
 
 
+# The results expected is only distinguished by the above
+# distinct Ortholog gene ID's, hence, indexed in that manner
 result_expected = {
-    # Test complete Panther record
-    "NCBIGene:6837": [
+    # Test complete Panther records
+    # (defective records do not get passed through to the result_expected)
+    "RGD:1564893": [
+        "HGNC:11477",
         "NCBITaxon:9606",
-        "NCBIGene:20933",
+        "NCBITaxon:10116",
+        "biolink:orthologous_to",
+        "RO:HOM0000017"
+    ],
+    "ZFIN:ZDB-GENE-040625-156": [
+        "HGNC:11477",
+        "NCBITaxon:9606",
+        "NCBITaxon:7955",
+        "biolink:orthologous_to",
+        "RO:HOM0000017"
+    ],
+    "WormBase:WBGene00007022": [
+        "HGNC:11477",
+        "NCBITaxon:9606",
+        "NCBITaxon:6239",
+        "biolink:orthologous_to",
+        "RO:HOM0000017"
+    ],
+    "FlyBase:FBgn0052971": [
+        "HGNC:11477",
+        "NCBITaxon:9606",
+        "NCBITaxon:7227",
+        "biolink:orthologous_to",
+        "RO:HOM0000017"
+    ],
+    "MGI:98446": [
+        "HGNC:11477",
+        "NCBITaxon:9606",
         "NCBITaxon:10090",
         "biolink:orthologous_to",
         "RO:HOM0000017"
@@ -125,9 +171,16 @@ result_expected = {
 def test_genes(basic_pl):
 
     gene = basic_pl[0]
+    ortholog = basic_pl[1]
 
     assert gene
-    assert gene.id in result_expected.keys()
+    assert ortholog
+    
+    # The test data mostly has the same human 'gene'
+    # but is distinguished by the orthology gene id
+    # hence the result_expected dictionary is now indexed thus...
+    assert ortholog.id in result_expected.keys()
+    assert gene.id == result_expected[ortholog.id][0]
 
     # 'category' is multivalued (an array)
     assert "biolink:Gene" in gene.category
@@ -136,14 +189,9 @@ def test_genes(basic_pl):
     assert "biolink:NamedThing" in gene.category
 
     # 'in_taxon' is multivalued (an array)
-    assert result_expected[gene.id][0] in gene.in_taxon
+    assert result_expected[ortholog.id][1] in gene.in_taxon
 
     assert "infores:entrez" in gene.source
-
-    ortholog = basic_pl[1]
-
-    assert ortholog
-    assert ortholog.id == result_expected[gene.id][1]
 
     # 'category' is multivalued (an array)
     assert "biolink:Gene" in ortholog.category
@@ -152,17 +200,23 @@ def test_genes(basic_pl):
     assert "biolink:NamedThing" in ortholog.category
 
     # 'in_taxon' is multivalued (an array)
-    assert result_expected[gene.id][2] in ortholog.in_taxon
+    assert result_expected[ortholog.id][2] in ortholog.in_taxon
 
     assert "infores:entrez" in ortholog.source
 
 
 def test_association(basic_pl):
+    
     association = basic_pl[2]
+    
     assert association
-    assert association.subject in result_expected.keys()
-    assert association.object == result_expected[association.subject][1]
-    assert association.predicate == result_expected[association.subject][3]
-    assert association.relation == result_expected[association.subject][4]
+    assert association.object in result_expected.keys()
+    
+    # The test data mostly has the same human 'gene' subject
+    # but is distinguished by the 'object' orthology gene id
+    # hence the result_expected dictionary is now indexed thus...
+    assert association.subject == result_expected[association.object][0]
+    assert association.predicate == result_expected[association.object][3]
+    assert association.relation == result_expected[association.object][4]
 
     assert "infores:panther" in association.source
