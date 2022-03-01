585b46710321c0bb131cbea7da9de69ae9389eb1
Kevin Schaper <kevinschaper@gmail.com>
2022-02-28 17:17:22 -0800

    A rough draft of the CTD ingest with logic brought over from Dipper.

---
+++ tests/unit/ctd/test_ctd_chemical_to_disease.py
@@ -0,0 +1,85 @@
+import pytest
+
+from biolink_model_pydantic.model import (
+    ChemicalEntity,
+    Disease,
+    ChemicalToDiseaseOrPhenotypicFeatureAssociation,
+    Predicate,
+)
+
+
+@pytest.fixture
+def source_name():
+    return "ctd_chemical_to_disease"
+
+
+@pytest.fixture
+def script():
+    return "./monarch_ingest/ctd/chemical_to_disease.py"
+
+
+@pytest.fixture
+def no_direct_evidence(mock_koza, source_name, script, global_table):
+    row = {'ChemicalName': '10074-G5', 'ChemicalID': 'C534883', 'CasRN': '', 'DiseaseName': 'Adenocarcinoma', 'DiseaseID': 'MESH:D000230', 'DirectEvidence': '', 'InferenceGeneSymbol': 'MYC', 'InferenceScore': '4.08', 'OmimIDs': '', 'PubMedIDs': '26432044'}
+    return mock_koza(
+        name=source_name,
+        data=iter([row]),
+        transform_code=script,
+        global_table=global_table,
+    )
+
+@pytest.fixture
+def marker_mechanism(mock_koza, source_name, script, global_table):
+    row = {'ChemicalName': '10,10-bis(4-pyridinylmethyl)-9(10H)-anthracenone', 'ChemicalID': 'C112297', 'CasRN': '', 'DiseaseName': 'Hyperkinesis', 'DiseaseID': 'MESH:D006948', 'DirectEvidence': 'marker/mechanism', 'InferenceGeneSymbol': '', 'InferenceScore': '', 'OmimIDs': '', 'PubMedIDs': '19098162'}
+    return mock_koza(
+        name=source_name,
+        data=iter([row]),
+        transform_code=script,
+        global_table=global_table,
+    )
+
+@pytest.fixture
+def therapeutic(mock_koza, source_name, script, global_table):
+    row = {'ChemicalName': '10,11-dihydro-10-hydroxycarbamazepine', 'ChemicalID': 'C039775', 'CasRN': '', 'DiseaseName': 'Epilepsy', 'DiseaseID': 'MESH:D004827', 'DirectEvidence': 'therapeutic', 'InferenceGeneSymbol': '', 'InferenceScore': '', 'OmimIDs': '', 'PubMedIDs': '17516704|123'}
+    return mock_koza(
+        name=source_name,
+        data=iter([row]),
+        transform_code=script,
+        global_table=global_table,
+    )
+
+
+def test_no_direct_evidence(no_direct_evidence):
+    entities = no_direct_evidence
+    assert len(entities) == 0
+
+
+def test_marker_mechanism_entities(marker_mechanism):
+    entities = marker_mechanism
+    assert entities
+    assert len(entities) == 3
+    chemical = [e for e in entities if isinstance(e, ChemicalEntity)][0]
+    disease = [e for e in entities if isinstance(e, Disease)][0]
+    association = [e for e in entities if isinstance(e, ChemicalToDiseaseOrPhenotypicFeatureAssociation)][0]
+    assert chemical
+    assert disease
+    assert association
+    assert association.predicate == Predicate.biomarker_for
+    assert 'PMID:19098162' in association.publications
+
+
+def test_therapeutic_entities(therapeutic):
+    entities = therapeutic
+    assert entities
+    assert len(entities) == 3
+    chemical = [e for e in entities if isinstance(e, ChemicalEntity)][0]
+    disease = [e for e in entities if isinstance(e, Disease)][0]
+    association = [e for e in entities if isinstance(e, ChemicalToDiseaseOrPhenotypicFeatureAssociation)][0]
+    assert chemical
+    assert disease
+    assert association
+    assert association.predicate == Predicate.treats
+    assert 'PMID:17516704' in association.publications
+    assert 'PMID:123' in association.publications
+
+
