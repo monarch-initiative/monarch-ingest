ab2d48d7304c788940eb61efa62ce71553036e28
glass-ships <glass.ships@outlook.com>
2022-04-18 17:18:25 -0600

    dont need stream handler i guess

--- monarch_ingest/mgi/publication_to_gene.py
+++ monarch_ingest/mgi/publication_to_gene.py
@@ -8,6 +8,9 @@ from biolink_model_pydantic.model import (
 )
 from koza.cli_runner import koza_app
 
+import logging
+logger = logging.getLogger(__name__)
+
 source_name = "mgi_publication_to_gene"
 
 row = koza_app.get_row(source_name)
@@ -37,3 +40,6 @@ for pub_id in pub_ids:
     )
 
     koza_app.write(association)
+
+logger.info("HELLO? IS IT ME YOU'RE LOOKING FOR?")
+logger.debug("HELLO FROM THE OTHER SIIIIIIIDE")
