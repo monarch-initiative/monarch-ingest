c5dba487c2865c01182f3b322c93c65f1510b34a
Richard Bruskiewich <richard.bruskiewich@delphinai.com>
2022-05-06 16:18:23 -0700

    Additional significant iteration on ingest: missing OBO-derived phenotype mapping table

--- monarch_ingest/ingests/dictybase/gene_to_phenotype.py
+++ monarch_ingest/ingests/dictybase/gene_to_phenotype.py
@@ -1,36 +1,36 @@
 import uuid
 
 from biolink_model_pydantic.model import (
-    Gene,
     GeneToPhenotypicFeatureAssociation,
-    PhenotypicFeature,
     Predicate,
 )
 from koza.cli_runner import koza_app
+from .utils import parse_gene_id, parse_phenotypes
 
 source_name = "dictybase_gene_to_phenotype"
 
-#   - 'Systematic Name'
-#   - 'Strain Descriptor'
-#   - 'Associated gene(s)' - may be locus identifiers or gene symbols
-#   - 'Phenotypes'
 row = koza_app.get_row(source_name)
-gene_ids = koza_app.get_map("dictybase_gene_to_symbol")
 
-genes: str = row['Associated gene(s)']
-if genes and len(genes) == 1:
-    gene_id = genes
-    gene = Gene(id="dictyBase:" + row["Gene systematic ID"], source="infores:dictybase")
+gene_names_to_ids = koza_app.get_map("dictybase_gene_names_to_ids")
+phenotype_names_to_ids = koza_app.get_map("dictybase_phenotype_names_to_ids")
 
-    phenotypes = row["Phenotypes"]
-    phenotype = PhenotypicFeature(id=row["FYPO ID"], source="infores:dictybase")
-    association = GeneToPhenotypicFeatureAssociation(
-        id="uuid:" + str(uuid.uuid1()),
-        subject=gene.id,
-        predicate=Predicate.has_phenotype,
-        object=phenotype.id,
-        relation=koza_app.translation_table.resolve_term("has phenotype"),
-        source="infores:dictybase"
-    )
+gene_id: str = parse_gene_id(row, gene_names_to_ids)
+if gene_id:
 
-    koza_app.write(association)
+    # Parse out list of phenotypes...
+    phenotypes = parse_phenotypes(row, phenotype_names_to_ids)
+
+    for phenotype_id in phenotypes:
+        # Create one S-P-O statement per phenotype
+        # TODO: how do we capture the 'Strain Descriptor' (genotype) context of
+        #       Dictylostelium via which a (mutant) gene (allele) is tied to its phenotype?
+        association = GeneToPhenotypicFeatureAssociation(
+            id="uuid:" + str(uuid.uuid1()),
+            subject=gene_id,
+            predicate=Predicate.has_phenotype,
+            object=phenotype_id,
+            relation=koza_app.translation_table.resolve_term("has phenotype"),
+            source="infores:dictybase"
+        )
+
+        koza_app.write(association)
---
+++ monarch_ingest/ingests/dictybase/utils.py
@@ -0,0 +1,79 @@
+"""
+A few Dictybase parse utility functions
+"""
+from typing import Optional, Dict, List
+import logging
+
+logger = logging.getLogger()
+
+
+def parse_gene_id(row: Dict, gene_names_to_ids) -> Optional[str]:
+    """
+    Parses the 'Associated gene(s)' field of the data row
+    to extract a dictybase gene ID associated with the row.
+
+    :param row: dictionary with an 'Associated gene(s)' field
+    :param gene_names_to_ids: 'Gene Name' to 'Gene ID' mappings dictionary
+    :return: string corresponding to a Dictybase gene CURIE
+    """
+    gene_field: str = row["Associated gene(s)"]
+
+    # sanity check: probably won't happen but empty gene field?
+    if not gene_field:
+        logger.warning(f"Input record has an empty 'Associated gene(s)' field?")
+        return None
+
+    # Split out multiple genes
+    genes = gene_field.split('|')
+
+    if len(genes) != 1:
+        # the current Dictybase Ingest policy is
+        # to only process entries with single genes
+        logger.debug(f"Input record has multiple genes assigned for a given strain/phenotype... Ignoring?")
+        return None
+
+    # map gene symbols to proper ids
+    gene_name = genes.pop().strip()
+
+    # Otherwise, attempt to map the gene symbol given into a gene identifier
+    try:
+        gene_id = gene_names_to_ids[gene_name]['Gene ID']
+    except KeyError:
+        logger.warning(f"Gene Name '{gene_name}' mapping onto a Gene ID is unknown")
+        return None
+
+    return "dictyBase:" + gene_id
+
+
+def parse_phenotypes(row: Dict, phenotype_names_to_ids: Dict) -> List[str]:
+    """
+    Parses the 'Phenotypes' field of the data row
+    to extract a dictybase phenotypes associated
+    with the given data row (Dictybase Strain).
+
+    :param row: dictionary with an 'Phenotypes' field
+    :param phenotype_names_to_ids: 'Phenotype Name' to 'Phenotype ID' mappings dictionary
+    :return: List of UPHENO-compatible phenotypes.
+    """
+    phenotypes_field: str = row["Phenotypes"]
+
+    # sanity check: probably won't happen but empty gene field?
+    if not phenotypes_field:
+        logger.warning(f"Input record has an empty 'Phenotypes' field?")
+        return []
+
+    # Split out multiple genes
+    phenotypes: List[str] = phenotypes_field.split('|')
+
+    # Clean up leading and trailing spaces
+    phenotypes = [phenotype.strip() for phenotype in phenotypes]
+
+    phenotype_ids: List[str] = list()
+    for phenotype_name in phenotypes:
+        try:
+            phenotype_ids.append(phenotype_names_to_ids[phenotype_name]['id'])
+        except KeyError:
+            logger.warning(f"Phenotype Name '{phenotype_name}' mapping onto a Phenotype ID is unknown")
+            return []
+
+    return phenotype_ids
