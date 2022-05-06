34f2a47fad5da465f19af1e4152ac62878a7f698
Richard Bruskiewich <richard.bruskiewich@delphinai.com>
2022-05-06 09:11:45 -0700

    another tiny iteration

--- monarch_ingest/ingests/dictybase/gene_to_phenotype.py
+++ monarch_ingest/ingests/dictybase/gene_to_phenotype.py
@@ -17,15 +17,20 @@ source_name = "dictybase_gene_to_phenotype"
 row = koza_app.get_row(source_name)
 gene_ids = koza_app.get_map("dictybase_gene_to_symbol")
 
-gene = Gene(id="dictyBase:" + row["Gene systematic ID"], source="infores:dictybase")
-phenotype = PhenotypicFeature(id=row["FYPO ID"], source="infores:dictybase")
-association = GeneToPhenotypicFeatureAssociation(
-    id="uuid:" + str(uuid.uuid1()),
-    subject=gene.id,
-    predicate=Predicate.has_phenotype,
-    object=phenotype.id,
-    relation=koza_app.translation_table.resolve_term("has phenotype"),
-    source="infores:dictybase"
-)
+genes: str = row['Associated gene(s)']
+if genes and len(genes) == 1:
+    gene_id = genes
+    gene = Gene(id="dictyBase:" + row["Gene systematic ID"], source="infores:dictybase")
 
-koza_app.write(association)
+    phenotypes = row["Phenotypes"]
+    phenotype = PhenotypicFeature(id=row["FYPO ID"], source="infores:dictybase")
+    association = GeneToPhenotypicFeatureAssociation(
+        id="uuid:" + str(uuid.uuid1()),
+        subject=gene.id,
+        predicate=Predicate.has_phenotype,
+        object=phenotype.id,
+        relation=koza_app.translation_table.resolve_term("has phenotype"),
+        source="infores:dictybase"
+    )
+
+    koza_app.write(association)
