0bdc0c424fc820947e7db0c5bef455126a6730d2
Richard Bruskiewich <richard.bruskiewich@delphinai.com>
2022-05-06 17:35:11 -0700

    Dicty Phenotype OBO to TSV mapping file code

---
+++ monarch_ingest/util/dicty_phenotype_obo_to_map.py
@@ -0,0 +1,29 @@
+from typing import Dict
+from json import load
+
+
+def to_curie(uri: str):
+    return uri.replace("http://purl.obolibrary.org/obo/DDPHENO_", "DDPHENO:")
+
+
+with open("../../data/dictybase/dicty_phenotypes.json") as phenotype_file_json:
+    dbpheno = load(phenotype_file_json)
+
+nodes: Dict = dbpheno["graphs"][0]["nodes"]
+
+name_to_id: Dict[str, str] = dict()
+
+for node in nodes:
+
+    if not ("lbl" in node and "id" in node):
+        continue
+
+    name = node["lbl"]
+    uri = node["id"]
+    curie = to_curie(uri)
+    name_to_id[name] = curie
+
+with open("../../data/dictybase/dicty_phenotypes.tsv", mode="w") as phenotype_file_tsv:
+    print(f"name\tid", file=phenotype_file_tsv)
+    for name, identifier in name_to_id.items():
+        print(f"{name}\t{identifier}", file=phenotype_file_tsv)
