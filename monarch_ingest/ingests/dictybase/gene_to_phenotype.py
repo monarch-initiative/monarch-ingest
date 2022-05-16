6bda0d15ad904cc76a16cc07856357f1689fea4f
Richard Bruskiewich <richard.bruskiewich@delphinai.com>
2022-05-16 16:12:35 -0700

    Implemented an independent Dictybase Gene ingest

--- monarch_ingest/ingests/dictybase/gene_to_phenotype.py
+++ monarch_ingest/ingests/dictybase/gene_to_phenotype.py
@@ -1,4 +1,5 @@
 import uuid
+from typing import Optional, Tuple
 
 from biolink_model_pydantic.model import (
     GeneToPhenotypicFeatureAssociation,
@@ -14,8 +15,8 @@ row = koza_app.get_row(source_name)
 gene_names_to_ids = koza_app.get_map("dictybase_gene_names_to_ids")
 phenotype_names_to_ids = koza_app.get_map("dictybase_phenotype_names_to_ids")
 
-gene_id: str = parse_gene_id(row, gene_names_to_ids)
-if gene_id:
+gene_identifier: Optional[Tuple[str, str]] = parse_gene_id(row, gene_names_to_ids)
+if gene_identifier:
 
     # Parse out list of phenotypes...
     phenotypes = parse_phenotypes(row, phenotype_names_to_ids)
@@ -26,7 +27,7 @@ if gene_id:
         #       Dictylostelium via which a (mutant) gene (allele) is tied to its phenotype?
         association = GeneToPhenotypicFeatureAssociation(
             id="uuid:" + str(uuid.uuid1()),
-            subject=gene_id,
+            subject=gene_identifier[0],  # gene[0] is the resolved gene ID
             predicate=Predicate.has_phenotype,
             object=phenotype_id,
             relation=koza_app.translation_table.resolve_term("has phenotype"),
---
+++ tests/unit/dictybase/test_dictybase_gene.py
@@ -0,0 +1,151 @@
+from typing import Dict, List
+import pytest
+
+from biolink_model_pydantic.model import Gene
+from monarch_ingest.ingests.dictybase.utils import parse_gene_id
+
+
+@pytest.fixture
+def map_cache() -> Dict:
+    """
+    :return: Multi-level mock map_cache of test Dictybase Gene Names to Identifiers.
+    """
+    dictybase_gene_names_to_ids = {
+        "cbpC": {"GENE ID": "DDB_G0283613"},
+        "DDB_G0267364_RTE": {"GENE ID": "DDB_G0267364"},
+        "argE": {"GENE ID": "DDB_G0267380"},
+        "DDB_G0269812": {"GENE ID": "DDB_G0269812"}
+    }
+    return {
+        "dictybase_gene_names_to_ids": dictybase_gene_names_to_ids
+    }
+
+
+@pytest.mark.parametrize(
+    "query",
+    [
+        (
+            {
+                # empty gene field
+                "Associated gene(s)": None
+            },
+            None
+        ),
+        (
+            {
+                # Multiple gene mappings - ignored
+                "Associated gene(s)": "zplC-1 | zplC-2"
+            },
+            None
+        ),
+        (
+            {
+                # Multiple gene mappings - ignored
+                "Associated gene(s)": "unknown_gene_name"
+            },
+            None
+        ),
+        (
+            {
+                # gene symbol
+                "Associated gene(s)": "argE"
+            },
+            ("dictyBase:DDB_G0267380", "argE")
+        ),
+        (
+            {
+                # gene id like (Retro Transposable Element?)
+                "Associated gene(s)": "DDB_G0267364_RTE"
+            },
+            ("dictyBase:DDB_G0267364", "DDB_G0267364_RTE")
+        ),
+        (
+            {
+                # regular gene locus
+                "Associated gene(s)": "DDB_G0269812"
+            },
+            ("dictyBase:DDB_G0269812", "DDB_G0269812")
+        ),
+    ]
+)
+def test_parse_gene_id(query, map_cache):
+    gene = parse_gene_id(query[0], map_cache["dictybase_gene_names_to_ids"])
+    assert gene == query[1]
+
+
+@pytest.fixture
+def source_name():
+    """
+    :return: string source name of Dictybase Gene to Phenotype ingest
+    """
+    return "dictybase_gene"
+
+
+@pytest.fixture
+def script():
+    """
+    :return: string path to Dictybase Gene ngest script
+    """
+    return "./monarch_ingest/ingests/dictybase/gene.py"
+
+
+@pytest.fixture
+def test_row():
+    """
+    :return: Test Dictybase Gene input data row.
+    """
+    return {
+        "Systematic Name": "DBS0235594",
+        "Strain Descriptor": "CHE10",
+        "Associated gene(s)": "cbpC",
+        "Phenotypes": " decreased slug migration | aberrant spore morphology "
+    }
+
+
+@pytest.fixture
+def basic_dictybase(mock_koza, source_name, script, test_row, global_table, map_cache):
+    """
+    Mock Koza run for Dictybase Gene ingest.
+
+    :param mock_koza:
+    :param source_name:
+    :param test_row:
+    :param script:
+    :param global_table:
+    :param map_cache:
+
+    :return: mock_koza application
+    """
+    return mock_koza(
+        name=source_name,
+        data=iter([test_row]),
+        transform_code=script,
+        global_table=global_table,
+        map_cache=map_cache
+    )
+
+
+@pytest.mark.parametrize(
+    "cls", [Gene]
+)
+def confirm_one_of_each_classes(cls, basic_dictybase):
+    class_entities = [entity for entity in basic_dictybase if isinstance(entity, cls)]
+    assert class_entities
+    assert len(class_entities) == 1
+    assert class_entities[0]
+
+
+def test_dictybase_gene_ingest(basic_dictybase):
+    entity: List[Gene] = [
+        entity
+        for entity in basic_dictybase
+        if isinstance(entity, Gene)
+    ]
+    assert len(entity) == 1
+
+    assert entity[0]
+    assert entity[0].id == "dictyBase:DDB_G0283613"
+    assert entity[0].symbol == "cbpC"
+    assert entity[0].name == "cbpC"
+    assert "NCBITaxon:44689" in entity[0].in_taxon
+    assert "infores:dictybase" in entity[0].source
