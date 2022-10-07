8021de0f2715d74b114a3ee5c113508eda66a09f
Kevin Schaper <kevinschaper@gmail.com>
2022-10-06 22:35:48 -0700

    Update dictybase gene ingest to just their gene-information file

--- monarch_ingest/ingests/dictybase/gene.py
+++ monarch_ingest/ingests/dictybase/gene.py
@@ -1,7 +1,4 @@
-from typing import Optional, Tuple
-
 from koza.cli_runner import get_koza_app
-from monarch_ingest.ingests.dictybase.utils import parse_gene_id
 
 from biolink.pydanticmodel import Gene
 
@@ -9,18 +6,17 @@ koza_app = get_koza_app("dictybase_gene")
 
 row = koza_app.get_row()
 
-gene_names_to_ids = koza_app.get_map("dictybase_gene_names_to_ids")
-dicty_symbols_to_ncbi_genes = koza_app.get_map("dicty_symbols_to_ncbi_genes")
+synonyms = []
+if row['Synonyms'] is not None:
+    synonyms = row['Synonyms'].split(", ")
 
-gene_identifier: Optional[Tuple[str, str]] = parse_gene_id(row, gene_names_to_ids, dicty_symbols_to_ncbi_genes)
-if gene_identifier:
+gene = Gene(
+    id='dictyBase:' + row['GENE ID'],
+    symbol=row['Gene Name'],
+    name=row['Gene Name'],
+    synonym=synonyms,
+    in_taxon=["NCBITaxon:44689"],
+    provided_by=["infores:dictybase"]
+)
 
-    gene = Gene(
-        id=gene_identifier[0],
-        symbol=gene_identifier[1],
-        name=gene_identifier[1],
-        in_taxon=["NCBITaxon:44689"],
-        provided_by=["infores:dictybase"]
-    )
-
-    koza_app.write(gene)
+koza_app.write(gene)
