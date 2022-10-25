018e0055979ba8b67c07c128dced2a8890abcc03
Kevin Schaper <kevinschaper@gmail.com>
2022-10-25 13:08:33 -0700

    Refactored panther ingest to not need pre-processing

--- monarch_ingest/ingests/panther/genome_orthologs.py
+++ monarch_ingest/ingests/panther/genome_orthologs.py
@@ -8,49 +8,52 @@ from koza.cli_runner import get_koza_app
 
 from biolink.pydanticmodel import GeneToGeneHomologyAssociation
 
-from monarch_ingest.ingests.panther.orthology_utils import parse_gene
+from monarch_ingest.ingests.panther.orthology_utils import parse_gene, ncbitaxon_catalog
 
 logger = logging.getLogger(__name__)
 
 koza_app = get_koza_app("panther_genome_orthologs")
-
 row = koza_app.get_row()
 
-try:
-    # TODO: we don't current capture the taxon of the subject gene
-    #       nor the object ortholog. Maybe as a qualifier in Biolink 3.0?
+for row in koza_app.source:
+    if row['Gene'][0:5] in ncbitaxon_catalog and row['Ortholog'][0:5] in ncbitaxon_catalog:
 
-    species_and_gene_id = parse_gene(row['Gene'])
+        # TODO: we don't current capture the taxon of the subject gene
+        #       nor the object ortholog. Maybe as a qualifier in Biolink 3.0?
 
-    # unpack the species and gene id
-    gene_ncbitaxon, gene_id = species_and_gene_id
+        species_and_gene_id = parse_gene(row['Gene'])
 
-    species_and_ortholog_id = parse_gene(row['Ortholog'])
+        if species_and_gene_id is None:
+            continue
 
-    # unpack the orthogous gene id and its species
-    ortholog_ncbitaxon, ortholog_id = species_and_ortholog_id
+        # unpack the species and gene id
+        gene_ncbitaxon, gene_id = species_and_gene_id
 
-    # TODO: how do I discriminate between LDO and O? I don't care for now??
-    #       However, this may result in KGX record duplication?
-    # ortholog_type = row["Type of ortholog"]
-    predicate = "biolink:orthologous_to"
+        species_and_ortholog_id = parse_gene(row['Ortholog'])
 
-    # Instantiate the instance of Gene-to-Gene Homology Association
-    panther_ortholog_id = row["Panther Ortholog ID"]
-    association = GeneToGeneHomologyAssociation(
-        id=f"uuid:{str(uuid.uuid1())}",
-        subject=gene_id,
-        object=ortholog_id,
-        predicate=predicate,
-        has_evidence=[f"PANTHER.FAMILY:{panther_ortholog_id}"],
-        aggregator_knowledge_source=["infores:monarchinitiative"],
-        primary_knowledge_source="infores:panther"
-    )
+        # unpack the orthogous gene id and its species
+        if species_and_ortholog_id is None:
+            continue
 
-    # Write the captured Association out
-    koza_app.write(association)
+        ortholog_ncbitaxon, ortholog_id = species_and_ortholog_id
 
-except RuntimeError as rte:
-    # Skip the row - not of interest or error
-    # logger.debug(f"{str(rte)} in data row:\n\t'{str(row)}'")
-    pass
+        # TODO: how do I discriminate between LDO and O? I don't care for now??
+        #       However, this may result in KGX record duplication?
+        # ortholog_type = row["Type of ortholog"]
+        predicate = "biolink:orthologous_to"
+
+        # Instantiate the instance of Gene-to-Gene Homology Association
+        panther_ortholog_id = row["Panther Ortholog ID"]
+        association = GeneToGeneHomologyAssociation(
+            id=f"uuid:{str(uuid.uuid1())}",
+            subject=gene_id,
+            object=ortholog_id,
+            predicate=predicate,
+            has_evidence=[f"PANTHER.FAMILY:{panther_ortholog_id}"],
+            aggregator_knowledge_source=["infores:monarchinitiative"],
+            primary_knowledge_source="infores:panther"
+        )
+
+        # Write the captured Association out
+        koza_app.write(association)
+
--- monarch_ingest/ingests/panther/orthology_utils.py
+++ monarch_ingest/ingests/panther/orthology_utils.py
@@ -18,7 +18,7 @@ import logging
 
 logger = logging.getLogger()
 
-_ncbitaxon_catalog = {
+ncbitaxon_catalog = {
     # TODO: may need to further build up this catalog
     #       of species - just starting with the STRING DB list + Dictyostelium
     "HUMAN": "9606",
@@ -39,97 +39,13 @@ _ncbitaxon_catalog = {
     "YEAST": "4932",
 }
 
-
-ALL_ORTHOLOGS_FILE = "AllOrthologs"
-TARGET_SPECIES_ORTHOLOGS = "TargetOrthologs"
-
-
-def target_taxon(line: Optional[str]) -> bool:
-    if not line:
-        return False
-    line_parts = line.split("\t")
-    gene_parts = line_parts[0].split("|")
-    ortholog_parts = line_parts[1].split("|")
-
-    if gene_parts[0] in _ncbitaxon_catalog.keys() and ortholog_parts[0] in _ncbitaxon_catalog.keys():
-        return True
-    else:
-        return False
-
-
-def filter_panther_orthologs_file(
-        directory: str = '.',
-        source_filename: str = ALL_ORTHOLOGS_FILE,
-        target_filename: str = TARGET_SPECIES_ORTHOLOGS,
-        number_of_lines: int = 0
-) -> bool:
-    """
-    Filters contents of a Panther orthologs tar.gz archive against the target list of taxa.
-
-    :param directory: str, location of source data file
-    :param source_filename: str, source data file name
-    :param target_filename: str, target data file name
-    :param number_of_lines: int, number of lines parsed; 'all' lines parsed if omitted or set to zero
-    :return: bool, True if filtering was successful; False if unsuccessful
-    """
-    assert source_filename
-    assert target_filename
-    assert number_of_lines >= 0
-    print(
-        f"\nBegin file filtering '{number_of_lines if number_of_lines else 'all'}'" +
-        f" lines in '{source_filename}' at {datetime.now().isoformat()}. " +
-        f"\nPatience! This may take a little awhile!...", file=stderr
-    )
-    # A standard TAR file with a single entry identical in name to filename
-    source_tar_filename: str = f"{source_filename}.tar.gz"
-    source_tar_file_path: str = f"{directory}{sep}{source_tar_filename}"
-    target_file_path: str = f"{directory}{sep}{target_filename}"
-    n: int = 0
-    try:
-        with tar_open(source_tar_file_path, mode='r') as source_tar_file:
-            with open(target_file_path, mode='w', encoding='utf-8') as target_file:
-                # first member assumed to be the one and only target member
-                member = source_tar_file.next()
-                if not member:
-                    logger.error(f"filter_file() tar archive '{source_tar_filename}' is empty?")
-                    print(
-                        f"Failed preprocessing '{source_filename}' at {datetime.now().isoformat()}", file=stderr
-                    )
-                    return False
-                with source_tar_file.extractfile(member) as orthologs_reader:  # io.BufferedReader
-                    orthologs_file = TextIOWrapper(orthologs_reader)
-                    for line in orthologs_file:
-                        if target_taxon(line):
-                            print(line, file=target_file, end="")
-                            n += 1
-                        if number_of_lines and n > number_of_lines:
-                            break
-    except (ReadError, CompressionError) as e:
-        logger.error(f"filter_file() tar file access exception: {str(e)}")
-        print(f"Failed preprocessing '{source_filename}' at {datetime.now().isoformat()}", file=stderr)
-        return False
-
-    target_tar_filename: str = f"{target_filename}.tar.gz"
-    target_tar_file_path: str = f"{directory}{sep}{target_tar_filename}"
-    with tar_open(target_tar_file_path, mode='w:gz') as target_tar_file:
-        target_tarinfo: TarInfo = target_tar_file.gettarinfo(name=target_file_path, arcname=target_filename)
-        with open(target_file_path, mode='rb') as target_file:
-            target_tar_file.addfile(target_tarinfo, fileobj=target_file)
-
-    # delete the naked file
-    remove(target_file_path)
-
-    print(f"Finished filtering file '{source_filename}' at {datetime.now().isoformat()}", file=stderr)
-    return True
-
-
 def ncbitaxon_by_name(species_tag: str) -> Optional[str]:
     """
     Retrieves the NCBI Taxon ID of a given species, only if we are interested in it...
     :param species_tag:
     """
-    if species_tag in _ncbitaxon_catalog:
-        return f"NCBITaxon:{_ncbitaxon_catalog[species_tag]}"
+    if species_tag in ncbitaxon_catalog:
+        return f"NCBITaxon:{ncbitaxon_catalog[species_tag]}"
     else:
         # ncbitaxon_by_name(): taxon with this species_tag is not of interest to Monarch? Ignoring...
         return None
@@ -167,7 +83,7 @@ def parse_gene_id(gene_id_spec: str) -> Optional[str]:
     :param gene_id_spec: is assumed to be of form 'UniProtKB=<object_id>'
     """
     if not gene_id_spec:
-        raise RuntimeError(f"parse_gene_id(): Empty 'gene_id_spec'? Ignoring...")
+        return None
 
     spec_part = gene_id_spec.split("=")
 
@@ -176,9 +92,7 @@ def parse_gene_id(gene_id_spec: str) -> Optional[str]:
         prefix = get_biolink_curie_prefix(spec_part[0])
 
         if not prefix:
-            raise RuntimeError(
-                f"parse_gene_id(): Namespace '{spec_part[0]}' is not mappable to a canonical namespace? Ignoring..."
-            )
+            return None
         return f"{prefix}:{spec_part[1]}"
 
     elif len(spec_part) == 3 and spec_part[1] == "MGI":
@@ -186,11 +100,8 @@ def parse_gene_id(gene_id_spec: str) -> Optional[str]:
         # (is this the only one like this? Logger errors
         #  trapped by the RuntimeError below should answer this...)
         return f"{spec_part[1]}:{spec_part[2]}"
-
     else:
-        raise RuntimeError(
-            f"parse_gene_id(): Error parsing '{str(gene_id_spec)}'? Ignoring..."
-        )
+        return None
 
 
 def parse_gene(gene_entry: str) -> Optional[Tuple[str, str]]:
@@ -201,33 +112,26 @@ def parse_gene(gene_entry: str) -> Optional[Tuple[str, str]]:
     """
 
     if not gene_entry:
-        raise RuntimeError("parse_gene(): Empty 'gene_entry' argument?. Ignoring...")
+        return None
 
-    try:
-        species, gene_spec, _ = gene_entry.split("|")
-    except ValueError:
-        raise RuntimeError(
-            f"parse_gene(): Gene entry field '{str(gene_entry)}' has incorrect format. Ignoring..."
-        )
+    species, gene_spec, _ = gene_entry.split("|")
+
+    if not species or not gene_spec:
+        return None
 
     # get the NCBI Taxonomic identifier
     ncbitaxon_id = ncbitaxon_by_name(species)
 
     # Quietly ignore a species we don't know about? Only complain in debug mode though (otherwise...)
     if not ncbitaxon_id:
-        raise RuntimeError(
-            f"parse_gene(): Taxon '{str(species)}' in gene entry "
-            f"'{str(gene_entry)}' is not in target list. Ignoring..."
-        )
+        return None
 
     # get the gene identifier
     gene_id = parse_gene_id(gene_spec)
 
     # Quietly ignore a gene identifier we can't parse out? Only complain in debug mode though (otherwise...)
     if not gene_id:
-        raise RuntimeError(
-            f"parse_gene(): Gene identifier in gene entry '{str(gene_entry)}' cannot be parsed. Ignoring..."
-        )
+        return None
 
     return ncbitaxon_id, gene_id
 
