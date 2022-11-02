3e38c027cef8ce6025f7f925fce466b0347ac7a9
amc-corey-cox <69321580+amc-corey-cox@users.noreply.github.com>
2022-11-02 16:05:16 -0500

    Add basic BGee ingest

---
+++ monarch_ingest/ingests/bgee/gene_to_expression.py
@@ -0,0 +1,19 @@
+import uuid
+from koza.cli_runner import get_koza_app
+from biolink.pydanticmodel import GeneToExpressionSiteAssociation
+
+# The source name is used for reading and writing
+source_name = "bgee_gene_to_expression"
+
+koza_app = get_koza_app(source_name)
+
+# create your entities
+while (row := koza_app.get_row()) is not None:
+    association = GeneToExpressionSiteAssociation(
+        id="uuid:" + str(uuid.uuid1()),
+        subject=row['Gene ID'],
+        predicate='biolink:expressed_in',
+        object=row['Anatomical entity ID'],
+        aggregator_knowledge_source=["infores:monarchinitiative", "infores:bgee"])
+
+    koza_app.write(association)
