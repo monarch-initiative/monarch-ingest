5307705323038509964834f6c0e15c55ab0c15c0
Kevin Schaper <kevinschaper@gmail.com>
2023-02-07 14:50:37 -0800

    Unpack phenio relations, pass the new file to closurizer

--- scripts/after_download.sh
+++ scripts/after_download.sh
@@ -6,3 +6,5 @@ zcat data/alliance/BGI_*.gz | jq '.data[].basicGeneticEntity.primaryId' | gzip >
 # Make an id, name map of DDPHENO terms
 sqlite3 -cmd ".mode tabs" -cmd ".headers on" data/dictybase/ddpheno.db "select subject as id, value as name from rdfs_label_statement where predicate = 'rdfs:label' and subject like 'DDPHENO:%'" > data/dictybase/ddpheno.tsv
 
+# Unpack the phenio relation graph file
+tar -xzf data/monarch/phenio-relation-graph.tar.gz -C data/monarch/
