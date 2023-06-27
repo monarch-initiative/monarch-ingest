e08257f4234e455dae32d0c0e4621ec2ec1212e9
Kevin Schaper <kevinschaper@gmail.com>
2023-06-27 09:26:11 -0700

    Change ctd predicate to affects

--- src/monarch_ingest/constants.py
+++ src/monarch_ingest/constants.py
@@ -5,6 +5,7 @@ INFORES_ORPHANET = "infores:orphanet"
 INFORES_MEDGEN = "infores:medgen"
 
 # predicates
+BIOLINK_AFFECTS = "biolink:affects"
 BIOLINK_CAUSES = "biolink:causes"
 BIOLINK_CONTRIBUTES_TO = "biolink:contributes_to"
 BIOLINK_GENE_ASSOCIATED_WITH_CONDITION = "biolink:gene_associated_with_condition"
