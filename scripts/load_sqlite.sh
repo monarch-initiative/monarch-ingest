669c41fd956e861223c21306c6ea502c319369da
Kevin Schaper <kevinschaper@gmail.com>
2023-08-25 11:19:01 -0700

    Add closure table to sqlite db, and subject/object indices for edges, denormalized edges and closure table

--- scripts/load_sqlite.sh
+++ scripts/load_sqlite.sh
@@ -18,6 +18,13 @@ sqlite3 -cmd ".mode tabs" output/monarch-kg.db ".import output/monarch-kg-denorm
 sqlite3 output/monarch-kg.db "CREATE TABLE closure (subject TEXT, predicate TEXT, object TEXT)"
 sqlite3 -cmd ".mode tabs" output/monarch-kg.db ".import data/monarch/phenio-relation-graph.tsv closure"
 
+echo "Creating indices..."
+sqlite3 output/monarch-kg.db "create index if not exists edges_subject_index on edges (subject)"
+sqlite3 output/monarch-kg.db "create index if not exists edges_object_index on edges (object)"
+sqlite3 output/monarch-kg.db "create index if not exists closure_subject_index on closure (subject)"
+sqlite3 output/monarch-kg.db "create index if not exists denormalized_edges_subject_index on denormalized_edges (subject)"
+sqlite3 output/monarch-kg.db "create index if not exists denormalized_edges_object_index on denormalized_edges (object)"
+
 echo "Cleaning up..."
 rm output/monarch-kg_*.tsv
 gzip --force output/qc/monarch-kg-dangling-edges.tsv
