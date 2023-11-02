83e462dd18d8d6025cbe99434bd1d02b7d8f62ba
Kevin Schaper <kevinschaper@gmail.com>
2023-11-01 18:57:04 -0700

    Fix release finding regex in update_lastest_relase.sh

--- scripts/update_latest_release.sh
+++ scripts/update_latest_release.sh
@@ -3,7 +3,7 @@
 # This script will push a local copy of the Solr, Sqlite and denormalized edge artifacts up to all
 # all copies of the bucket for a given release. It needs to be run from the root of the repo
 
-export RELEASE=$(gsutil ls gs://data-public-monarchinitiative/monarch-kg-dev/latest/ | grep -Eo "(\d){4}-(\d){2}-(\d){2}")
+export RELEASE=$(gsutil ls gs://data-public-monarchinitiative/monarch-kg-dev/latest/ | grep -o '[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}')
 echo "Updating Solr, SQLite and denormalized edge files for $RELEASE"
 
 gsutil cp output/monarch-kg.db.gz gs://monarch-archive/monarch-kg-dev/$RELEASE/
