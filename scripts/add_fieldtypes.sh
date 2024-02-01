4849b04ea7728536852eafda682742923559b274
Kevin Schaper <kevinschaper@gmail.com>
2024-01-31 17:02:38 -0800

    Adds lowercase keyword grounding field to Solr entity collection

--- scripts/add_fieldtypes.sh
+++ scripts/add_fieldtypes.sh
@@ -4,4 +4,5 @@ for core in entity association
 do
   curl -X POST -H 'Content-type:application/json' -d @scripts/text-fieldtype.json http://localhost:8983/solr/$core/schema
   curl -X POST -H 'Content-type:application/json' -d @scripts/autocomplete-fieldtype.json http://localhost:8983/solr/$core/schema
+  curl -X POST -H 'Content-type:application/json' -d @scripts/grounding-fieldtype.json http://localhost:8983/solr/$core/schema
 done
