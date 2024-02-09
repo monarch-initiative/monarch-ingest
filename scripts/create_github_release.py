e640237941ede62efe15d74f789b803fa375651c
glass-ships <glass.ships@outlook.com>
2024-02-09 11:25:00 -0700

    tweak asset dir

--- scripts/create_github_release.py
+++ scripts/create_github_release.py
@@ -4,7 +4,7 @@ import requests
 from pathlib import Path
 
 
-DIR = Path(__file__).parent
+DIR = Path(__file__).parent.parent / "output"
 GH_TOKEN = os.environ["GH_RELEASE_TOKEN"]
 UPLOAD_FILES = [
     "monarch-kg.tar.gz",
@@ -15,8 +15,8 @@ UPLOAD_FILES = [
 ]
 
 
-def create_release(kg_ver: str):
-    print(f"Creating release for {kg_ver}")
+def create_release(kg_version: str):
+    print(f"Creating release for {kg_version}")
     response = requests.post(
         url="https://api.github.com/repos/monarch-initiative/monarch-ingest/releases",
         headers={
@@ -25,9 +25,9 @@ def create_release(kg_ver: str):
             "X-GitHub-Api-Version": "2022-11-28",
         },
         json={
-            "tag_name": kg_ver,
+            "tag_name": kg_version,
             "target_commitish": "main",
-            "name": kg_ver,
+            "name": kg_version,
             # "body": "body",
             "draft": True,
             "prerelease": False,
@@ -37,12 +37,12 @@ def create_release(kg_ver: str):
     if response.status_code != 201:
         raise Exception(f"Failed to create release: {response.text}")
     release_id = response.json()["id"]
-    print(f"Release {kg_ver} created with id {release_id}")
+    print(f"Release {kg_version} created with id {release_id}")
     return release_id
 
 
-def upload_assets(kg_ver: str, release_id: str, files: list):
-    print(f"\nUploading assets for {kg_ver}")
+def upload_assets(kg_version: str, release_id: str, files: list):
+    print(f"\nUploading assets for {kg_version}")
     for file in files:
         upload_name = file.split("/")[-1]
         print(f"Uploading {file} as {upload_name}...")
@@ -62,9 +62,9 @@ def upload_assets(kg_ver: str, release_id: str, files: list):
             print(f"{file} uploaded successfully")
 
 
-def main(kg_ver: str):
-    release_id = create_release(kg_ver)
-    upload_assets(kg_ver, release_id, UPLOAD_FILES)
+def main(kg_version: str):
+    release_id = create_release(kg_version)
+    upload_assets(kg_version, release_id, UPLOAD_FILES)
 
 
 if __name__ == "__main__":
