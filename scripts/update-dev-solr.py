9c5e18a23129a089f9f4a301c7b521ff7b613f6c
glass-ships <glass.ships@outlook.com>
2024-02-20 18:10:09 -0700

    fix solr data permissions

--- scripts/update-dev-solr.py
+++ scripts/update-dev-solr.py
@@ -12,7 +12,8 @@ import sh
 # Stop the solr service from the manager node
 sh.gcloud(
     *"compute ssh --zone us-central1-a monarch-v3-dev-manager".split(),
-    f"--command='docker service scale monarch-v3_solr=0'",
+    "--",
+    "sudo docker service scale monarch-v3_solr=0",
 )
 
 
@@ -21,7 +22,10 @@ ssh_command = "compute ssh --zone us-central1-a monarch-v3-dev-solr"
 vm_command = """
 cd /srv/monarch-v3
 wget https://data.monarchinitiative.org/monarch-kg-dev/latest/solr.tar.gz -O solr.tar.gz
-tar -xzf solr.tar.gz
+sudo rm -rf data
+sudo tar -xzf solr.tar.gz
+sudo chgrp -R 8983 data
+sudo chmod -R g+w data
 """
 
 sh.gcloud(*ssh_command.split(), f"--command={vm_command}", _out=print)
@@ -30,5 +34,6 @@ sh.gcloud(*ssh_command.split(), f"--command={vm_command}", _out=print)
 # Start the solr service from the manager node
 sh.gcloud(
     *"compute ssh --zone us-central1-a monarch-v3-dev-manager".split(),
-    f"--command='docker service scale monarch-v3_solr=1'",
+    "--",
+    "sudo docker service scale monarch-v3_solr=1",
 )
