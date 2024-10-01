1e6aa28b3647bde91a257758caf7fb33ef2b1c75
Kevin Schaper <kevinschaper@gmail.com>
2024-09-30 17:02:36 -0700

    temporarily comment out rdf & neo4j-v4 builds to test on jenkins a little faster

--- scripts/kgx_transforms.sh
+++ scripts/kgx_transforms.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo "kgx transform to rdf"
-poetry run kgx transform -i tsv -c tar.gz -f nt -d gz -o output/monarch-kg.nt.gz output/monarch-kg.tar.gz
-
-echo "Building Neo4j 4 Artifact"
-docker rm -f neo4 || True
-mkdir neo4j-v4-data || True
-docker run -d --name neo4 -p7474:7474 -p7687:7687 -v neo4j-v4-data:/data --env NEO4J_AUTH=neo4j/admin neo4j:4.4
-poetry run kgx transform --transform-config neo4j-transform.yaml > neo4j-v4-load.log
-docker stop neo4
-docker run -v $(pwd)/output:/backup -v neo4j-v4-data:/data --entrypoint neo4j-admin neo4j:4.4 dump --to /backup/monarch-kg.neo4j.dump
+#echo "kgx transform to rdf"
+#poetry run kgx transform -i tsv -c tar.gz -f nt -d gz -o output/monarch-kg.nt.gz output/monarch-kg.tar.gz
+#
+#echo "Building Neo4j 4 Artifact"
+#docker rm -f neo4 || True
+#mkdir neo4j-v4-data || True
+#docker run -d --name neo4 -p7474:7474 -p7687:7687 -v neo4j-v4-data:/data --env NEO4J_AUTH=neo4j/admin neo4j:4.4
+#poetry run kgx transform --transform-config neo4j-transform.yaml > neo4j-v4-load.log
+#docker stop neo4
+#docker run -v $(pwd)/output:/backup -v neo4j-v4-data:/data --entrypoint neo4j-admin neo4j:4.4 dump --to /backup/monarch-kg.neo4j.dump
 
 echo "Building Neo4j 5 Artifact"
 docker rm -f neo5 || True
