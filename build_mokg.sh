c93daec9ab9c7292efb33c43aa7950767b3414af
DnlRKorn <6885702+DnlRKorn@users.noreply.github.com>
2025-07-07 16:37:35 -0400

    Update build_mokg.sh

--- build_mokg.sh
+++ build_mokg.sh
@@ -9,5 +9,3 @@ poetry run ingest merge --kg-name mokg
 #This is a command to convert the KGX output found in output/mokg.tar.gz into RDF output.
 #This is taken from https://github.com/monarch-initiative/monarch-ingest/blob/3d2b0b3693a021d7660214bcd81abc4409f8efe2/scripts/kgx_transforms.sh#L12
 #poetry run kgx transform -i tsv -c tar.gz -f nt -d gz -o output/monarch-kg.nt.gz output/monarch-kg.tar.gz
-
-cp output/mokg.tar.gz /projects/stars/var/data_services/mokg/mokg.tar.gz
